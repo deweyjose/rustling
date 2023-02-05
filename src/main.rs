9b09732710f86aa17a39ecb81e080a83c34b94b0
Dewey Jose <richard.e.jose@gmail.com>
2023-02-04 22:48:36 -0500

    shapes file arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use crate::health::Health::Alive;
+use crate::pattern::Pattern;
+use crate::pattern::PatternType;
+use std::env;
 use std::fs::File;
 use std::io::Read;
 
@@ -10,11 +14,21 @@ mod pattern;
 mod size;
 
 fn main() {
-    let mut file = File::open("shapes.json").unwrap();
-    let mut buff = String::new();
-    file.read_to_string(&mut buff).unwrap();
-
-    let configuration: Vec<pattern::PatternType> = serde_json::from_str(&buff).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let configuration: Vec<PatternType> =
+        if let Ok(mut file) = File::open(args.get(1).unwrap_or(&String::from("shapes.json"))) {
+            let mut buff = String::new();
+            file.read_to_string(&mut buff).unwrap();
+            serde_json::from_str(&buff).unwrap()
+        } else {
+            vec![PatternType {
+                name: String::from("default"),
+                patterns: vec![Pattern {
+                    name: String::from("default"),
+                    matrix: vec![vec![Alive, Alive, Alive]],
+                }],
+            }]
+        };
 
     let mut viewer = grid_viewer::init(configuration);
     viewer.render();
