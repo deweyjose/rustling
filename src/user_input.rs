47fc314cf877355fcc877059b0dc07cf0104100c
Dewey jose <richard.e.jose@gmail.com>
2026-01-04 12:47:22 -0500

    fix build errors

--- src/user_input.rs
+++ src/user_input.rs
@@ -6,10 +6,8 @@ use crossterm::event::{self, Event};
 /// Poll for a single crossterm event with a timeout.
 /// Returns Ok(None) when no event is available within the timeout.
 pub fn poll_event(timeout: Duration) -> io::Result<Option<Event>> {
-    if event::poll(timeout).map_err(|e| io::Error::other(e))? {
-        Ok(Some(
-            event::read().map_err(|e| io::Error::other(e))?,
-        ))
+    if event::poll(timeout).map_err(io::Error::other)? {
+        Ok(Some(event::read().map_err(io::Error::other)?))
     } else {
         Ok(None)
     }
